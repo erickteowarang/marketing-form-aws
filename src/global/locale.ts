649743df666cc24dea32dd9d631a64bb2f9f24ce
Unknown <Erick.teowarang@hotmail.com>
2020-02-25 02:56:07 +1100

    Add the react-select package and load strings in the locale file

--- src/global/locale.ts
+++ src/global/locale.ts
@@ -1,3 +1,11 @@
 export const ERROR_MESSAGES = {
     REQUIRED: 'This field is required',
+    INVALID_EMAIL: 'This email is invalid',
+    FIRST_NAME_LEN_ERROR: 'You must enter more than 3 characters',
 }
+
+export const INDUSTRY_OPTIONS = [
+    { value: 'retail', label: 'Retail' },
+    { value: 'construction', label: 'Construction' },
+    { value: 'other', label: 'Other' }
+]
